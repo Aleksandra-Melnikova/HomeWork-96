7d2b760154a04f20316790a3f983e8f397487017
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2025-02-25 18:22:32 +0600

    [API and FRONTEND] Removed unnecessary consoles

--- frontend/src/globalConstants.ts
+++ frontend/src/globalConstants.ts
@@ -1,2 +1,2 @@
 export const apiUrl = "http://localhost:8000";
-export const GOOGLE_CLIENT_ID = "50639629032-i10dkep0vkd58ieddj32fi0ctqpjekh7.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = ".apps.googleusercontent.com";
